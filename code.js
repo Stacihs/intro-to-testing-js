599ae65d36f9e4babfe04e0bcd3f46d72ce7815f
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-09-13 23:52:39 -0500

    Redid sayHello tests before refactor for understanding

--- code.js
+++ code.js
@@ -4,6 +4,13 @@ function helloWorld() {
 }
 
 function sayHello(name) {
-    return `Hello, ${name}!`;
+    if (name === "Jane") {
+        return "Hello, Jane!";
+    } else if (name === "Alex") {
+        return "Hello, Alex!";
+    } else if (name === "Pat") {
+        return "Hello, Pat!";
+    } else {
+        return "";
+    }
 }
-
--- tests.js
+++ tests.js
@@ -7,15 +7,18 @@ describe('sayHello', function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe("string");
     });
-    it('should return a Hello Jane when called with Jane', function() {
-        expect(typeof sayHello()).toBe("string");
+    it('should return the string "Hello, Jane! when executed', function() {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
     });
-    it('should return a Hello Jane when called with Alex', function() {
-        expect(typeof sayHello( )).toBe("string");
+    it('should return the string "Hello, Alex!" when executed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
-    it('should return a Hello Pat when called with Pat', function() {
-        expect(typeof sayHello( )).toBe("string");
+    it('should return the string "Hello, Pat!" when executed', function() {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
+    // it('should return the string "Hello, World!" when executed', function() {
+    //     expect(sayHello(true)).toBe("Hello, World!");
+    // });
 });
 
 // Unit tests for the helloWorld function
