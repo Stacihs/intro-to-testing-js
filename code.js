db69b1dc80f7901d6479851813f6d149da01beeb
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-09-13 23:57:54 -0500

    add tests and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -4,13 +4,5 @@ function helloWorld() {
 }
 
 function sayHello(name) {
-    if (name === "Jane") {
-        return "Hello, Jane!";
-    } else if (name === "Alex") {
-        return "Hello, Alex!";
-    } else if (name === "Pat") {
-        return "Hello, Pat!";
-    } else {
-        return "";
-    }
+    return `Hello, ${name}!`;
 }
--- tests.js
+++ tests.js
@@ -16,9 +16,6 @@ describe('sayHello', function() {
     it('should return the string "Hello, Pat!" when executed', function() {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
-    // it('should return the string "Hello, World!" when executed', function() {
-    //     expect(sayHello(true)).toBe("Hello, World!");
-    // });
 });
 
 // Unit tests for the helloWorld function
